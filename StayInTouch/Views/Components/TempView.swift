b9bc25bcf7c891eb4d472ed296bc18c413d30025
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2023-06-15 17:17:31 -0400

    Added git ignore

---
+++ StayInTouch/Views/Components/TempView.swift
@@ -0,0 +1,20 @@
+//
+//  TempView.swift
+//  StayInTouch
+//
+//  Created by Sparsh Paliwal on 6/15/23.
+//
+
+import SwiftUI
+
+struct TempView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TempView_Previews: PreviewProvider {
+    static var previews: some View {
+        TempView()
+    }
+}
